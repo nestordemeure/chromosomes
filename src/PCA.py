35a06880acbd73cd3db60d6fb3ec282e936181b8
Nestor Demeure <nestor@nestor-HP-ProBook-650-G1>
2018-09-30 12:16:04 +0200

    got rid of dead code

--- src/PCA.py
+++ src/PCA.py
@@ -137,38 +137,3 @@ composant_number = 80
 dataset = load_data(input_folder + "preprocessed/" + "training/" + "compressed/" + "dataset.pfull")
 #displays_dataset(dataset)
 
-#--------------------------------------------------------------------------------------------------
-
-def get_unlabeled_training_sample(input_folder, sample_size):
-    """returns an iterator with sample_size element per category in a label*pictures list"""
-    for path in glob.glob(input_folder + "*.p"):
-        label, pictures = load_data(path)
-        sample = random.sample(pictures, sample_size)
-        yield from sample
-    print("Sampling is finished.", flush=True)
-
-#preprocess_data(input_folder, save_folder)
-
-#sample = list(get_unlabeled_training_sample(save_folder, sample_size))
-#save_data(sample, "./sample.p")
-#sample = load_data("./sample.p")
-
-# see also Incremental PCA to work on the full dataset while not burning the memory
-# http://scikit-learn.org/stable/auto_examples/decomposition/plot_incremental_pca.html#sphx-glr-auto-examples-decomposition-plot-incremental-pca-py
-#pca = PCA(n_components=100)
-#pca.fit(sample)
-#save_data(pca, "./pca.p")
-#pca = load_data("./pca.p")
-
-def compress_data(input_folder, output_folder, pca, composant_number):
-    variances = pca.explained_variance_ratio_
-    variance_represented = sum(variances[0:composant_number])
-    print("You can represent {t}% of the original variance using {n} composants.".format(n=composant_number, t=100*variance_represented))
-    for path in glob.glob(input_folder + "*.p"):
-        label, pictures = load_data(path)
-        new_pictures = list(map(lambda tab: tab[0:composant_number], pca.transform(pictures)))
-        new_path = path.replace(input_folder, output_folder)
-        save_data((label, new_pictures), new_path)
-    print("Data has been reduced to {n} composants using the given pca.".format(n=composant_number), flush=True)
-
-#compress_data(save_folder, pca_folder, pca, 80)
